930ec8a220d12f76987f849f8224c673566120be
Peter Reutemann <fracpete@gmail.com>
2024-02-27 16:13:38 +1300

    sending feedback that history has been reset

--- 2024-02-19_cuda11.7/predict_redis.py
+++ 2024-02-19_cuda11.7/predict_redis.py
@@ -62,6 +62,7 @@ def process_text(msg_cont):
         if (not args.no_history) and (text.strip() == "RESET"):
             text = ""
             n_turn = 0
+            output_text = "History has been reset"
             log("Resetting history")
 
         if len(text) > 0:
